b52ebe44c264306dd1ae0a259ba0cfbd409296dc
Mark Bukhner <markbuxner@mail.ru>
2021-10-10 02:11:00 +0700

    Task_1_2_1 complete.

---
+++ Task_1_2_1/src/main/java/ru/nsu/alowator/Stack.java
@@ -0,0 +1,63 @@
+package ru.nsu.alowator;
+
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public class Stack<T> {
+
+    private int count = 0;
+    private T[] arr;
+
+    final private int INITIAL_ARR_SIZE = 16;
+    final private int ARR_MULTIPLIER = 2;
+
+    @SuppressWarnings("unchecked")
+    public Stack() {
+        arr = (T[]) new Object[INITIAL_ARR_SIZE];
+
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public void push(T x) {
+        resize(count + 1);
+        arr[count] = x;
+        count += 1;
+    }
+
+    public T pop() {
+        if (count <= 0) {
+            throw new NoSuchElementException();
+        }
+        count -= 1;
+        return arr[count];
+    }
+
+    public void pushStack(Stack<T> stack) {
+        this.resize(this.count  + stack.count);
+        System.arraycopy(stack.arr, 0, this.arr, this.count, stack.count);
+        this.count += stack.count;
+    }
+
+    public Stack<T> popStack(int size) {
+        if (size > count || size < 0) {
+            throw new NoSuchElementException();
+        }
+
+        Stack<T> stack = new Stack<>();
+        stack.resize(size);
+        System.arraycopy(this.arr, 0, stack.arr, 0, size);
+        stack.count = size;
+        this.count -= size;
+        return stack;
+    }
+
+    private void resize(int toSize)  {
+        while (arr.length < toSize) {
+            arr = Arrays.copyOf(arr, arr.length * ARR_MULTIPLIER);
+        }
+    }
+}
---
+++ Task_1_2_1/src/test/java/ru/nsu/alowator/StackTest.java
@@ -0,0 +1,148 @@
+package ru.nsu.alowator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+
+class StackTest {
+
+    class CustomClass{};
+
+    Stack<Integer> integerStack;
+
+    @BeforeEach
+    void stack_init() {
+        integerStack = new Stack<>();
+    }
+
+
+    @Test
+    void push_pop_integer() {
+        integerStack.push(8);
+        integerStack.push(0);
+        integerStack.push(-15);
+
+        Assertions.assertEquals(-15, integerStack.pop());
+        Assertions.assertEquals(0, integerStack.pop());
+        Assertions.assertEquals(8, integerStack.pop());
+    }
+
+    @Test
+    void push_pop_bigInteger() {
+        final int bigTestArraySize = 1000000;
+        Integer[] arr = new Integer[bigTestArraySize];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i * 3;
+            integerStack.push(arr[i]);
+        }
+        for (int i = arr.length - 1; i >= 0; i--) {
+            Assertions.assertEquals(arr[i], integerStack.pop());
+        }
+
+    }
+
+    @Test
+    void push_pop_customType() {
+        Stack<CustomClass> stack = new Stack<>();
+        CustomClass customClass = new CustomClass();
+        stack.push(customClass);
+
+        Assertions.assertEquals(customClass, stack.pop());
+    }
+
+    @Test
+    void pop_exception() {
+        integerStack.push(1);
+        integerStack.push(2);
+        integerStack.pop();
+        integerStack.pop();
+        Assertions.assertThrows(NoSuchElementException.class, () -> {integerStack.pop();});
+    }
+
+
+    @Test
+    void count_bigInteger() {
+        final int bigTestArraySize = 1000000;
+        Integer[] arr = new Integer[bigTestArraySize];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i * 3;
+            integerStack.push(arr[i]);
+            Assertions.assertEquals(i + 1, integerStack.count());
+        }
+    }
+
+
+    @Test
+    void pushStack_integer() {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        integerStack.pushStack(stack);
+
+        Assertions.assertEquals(3, integerStack.pop());
+        Assertions.assertEquals(2, integerStack.pop());
+        Assertions.assertEquals(1, integerStack.pop());
+    }
+
+    @Test
+    void pushStack_empty() {
+        Stack<Integer> stack = new Stack<>();
+        integerStack.pushStack(stack);
+
+        Assertions.assertEquals(0, integerStack.count());
+    }
+
+    @Test
+    void pushStack_customType() {
+        Stack<CustomClass> stack1 = new Stack<>();
+        Stack<CustomClass> stack2 = new Stack<>();
+        CustomClass customClass = new CustomClass();
+        stack2.push(customClass);
+
+        stack1.pushStack(stack2);
+
+        Assertions.assertEquals(customClass, stack1.pop());
+    }
+
+
+    @Test
+    void popStack_integer() {
+        integerStack.push(1);
+        integerStack.push(2);
+        integerStack.push(3);
+
+        Stack<Integer> stack = integerStack.popStack(3);
+
+        Assertions.assertEquals(3, stack.pop());
+        Assertions.assertEquals(2, stack.pop());
+        Assertions.assertEquals(1, stack.pop());
+        Assertions.assertEquals(0, integerStack.count());
+    }
+
+    @Test
+    void popStack_empty() {
+        Stack<Integer> stack = integerStack.popStack(0);
+        Assertions.assertEquals(0, stack.count());
+    }
+
+    @Test
+    void popStack_customType() {
+        Stack<CustomClass> stack1 = new Stack<>();
+        CustomClass customClass = new CustomClass();
+        stack1.push(customClass);
+
+        Stack<CustomClass> stack2 = stack1.popStack(1);
+
+        Assertions.assertEquals(customClass, stack2.pop());
+    }
+
+    @Test
+    void popStack_exception() {
+        Assertions.assertThrows(NoSuchElementException.class, () -> {integerStack.popStack(-1);});
+    }
+
+}
