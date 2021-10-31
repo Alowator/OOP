0a9279bf0ed8895cf79d1f4af79ea01841690f91
Mark Bukhner <markbuxner@mail.ru>
2021-10-31 15:48:28 +0700

    Initial commit

---
+++ Task_1_4_1/src/main/java/ru/nsu/alowator/Calculator.java
@@ -0,0 +1,4 @@
+package ru.nsu.alowator;
+
+public class Calculator {
+}
---
+++ Task_1_4_1/src/test/java/ru/nsu/alowator/CalculatorTest.java
@@ -0,0 +1,7 @@
+package ru.nsu.alowator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+}
