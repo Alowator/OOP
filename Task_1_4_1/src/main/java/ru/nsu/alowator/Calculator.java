69c17d4d18d21586724d63b75c0609ad663f5eff
Mark Bukhner <markbuxner@mail.ru>
2021-10-31 16:28:37 +0700

    Add base operations

--- Task_1_4_1/src/main/java/ru/nsu/alowator/Calculator.java
+++ Task_1_4_1/src/main/java/ru/nsu/alowator/Calculator.java
@@ -1,4 +1,43 @@
 package ru.nsu.alowator;
 
+import java.io.StringReader;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Calculator {
+
+    private Scanner scanner;
+
+    public double calculate(String input) {
+        scanner = new Scanner(input);
+        return nextExpr();
+    }
+
+    private double nextExpr() {
+        String expr = scanner.next();
+        switch (expr) {
+            case "+": return add(nextExpr(), nextExpr());
+            case "-": return sub(nextExpr(), nextExpr());
+            case "*": return mul(nextExpr(), nextExpr());
+            case "/": return div(nextExpr(), nextExpr());
+            default: return Double.parseDouble(expr);
+        }
+    }
+
+    private double add(double a, double b) {
+        return a + b;
+    }
+
+    private double sub(double a, double b) {
+        return a - b;
+    }
+
+    private double mul(double a, double b) {
+        return a * b;
+    }
+
+    private double div(double a, double b) {
+        return a / b;
+    }
+
 }
--- Task_1_4_1/src/test/java/ru/nsu/alowator/CalculatorTest.java
+++ Task_1_4_1/src/test/java/ru/nsu/alowator/CalculatorTest.java
@@ -1,7 +1,32 @@
 package ru.nsu.alowator;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+
+    Calculator calculator;
+
+    @BeforeEach
+    void calculator() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    void calculate_value() {
+        assertEquals(10.5, calculator.calculate("10.5"));
+    }
+
+    @Test
+    void calculate_negativeValue() {
+        assertEquals(-10.0, calculator.calculate("-10"));
+    }
+
+    @Test
+    void calculate_add() {
+        assertEquals(8, calculator.calculate("+ 5 3"));
+    }
 
 }
