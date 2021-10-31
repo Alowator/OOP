f9dd7006e54e0ea828a233c61a53ce701bbe8dee
Mark Bukhner <markbuxner@mail.ru>
2021-10-31 17:20:09 +0700

    Complete Task 1_4_1

--- Task_1_4_1/src/main/java/ru/nsu/alowator/Calculator.java
+++ Task_1_4_1/src/main/java/ru/nsu/alowator/Calculator.java
@@ -1,43 +1,68 @@
 package ru.nsu.alowator;
 
-import java.io.StringReader;
-import java.util.Objects;
 import java.util.Scanner;
 
-public class Calculator {
+public final class Calculator {
 
-    private Scanner scanner;
-
-    public double calculate(String input) {
-        scanner = new Scanner(input);
-        return nextExpr();
+    public static double calculate(String input) {
+        Scanner scanner = new Scanner(input);
+        return nextExpr(scanner);
     }
 
-    private double nextExpr() {
+    public static double calculate(Scanner scanner) {
+        return nextExpr(scanner);
+    }
+
+    private static double nextExpr(Scanner scanner) {
         String expr = scanner.next();
-        switch (expr) {
-            case "+": return add(nextExpr(), nextExpr());
-            case "-": return sub(nextExpr(), nextExpr());
-            case "*": return mul(nextExpr(), nextExpr());
-            case "/": return div(nextExpr(), nextExpr());
-            default: return Double.parseDouble(expr);
-        }
+        return switch (expr) {
+            case "+" -> add(nextExpr(scanner), nextExpr(scanner));
+            case "-" -> sub(nextExpr(scanner), nextExpr(scanner));
+            case "*" -> mul(nextExpr(scanner), nextExpr(scanner));
+            case "/" -> div(nextExpr(scanner), nextExpr(scanner));
+            case "log" -> log(nextExpr(scanner));
+            case "pow" -> pow(nextExpr(scanner), nextExpr(scanner));
+            case "sqrt" -> sqrt(nextExpr(scanner));
+            case "sin" -> sin(nextExpr(scanner));
+            case "cos" -> cos(nextExpr(scanner));
+            default -> Double.parseDouble(expr);
+        };
     }
 
-    private double add(double a, double b) {
+    private static double add(double a, double b) {
         return a + b;
     }
 
-    private double sub(double a, double b) {
+    private static double sub(double a, double b) {
         return a - b;
     }
 
-    private double mul(double a, double b) {
+    private static double mul(double a, double b) {
         return a * b;
     }
 
-    private double div(double a, double b) {
+    private static double div(double a, double b) {
         return a / b;
+    }
+
+    private static double log(double a) {
+        return Math.log(a);
+    }
+
+    private static double pow(double a, double b) {
+        return Math.pow(a, b);
+    }
+
+    private static double sqrt(double a) {
+        return Math.sqrt(a);
+    }
+
+    private static double sin(double a) {
+        return Math.sin(a);
+    }
+
+    private static double cos(double a) {
+        return Math.cos(a);
     }
 
 }
--- Task_1_4_1/src/test/java/ru/nsu/alowator/CalculatorTest.java
+++ Task_1_4_1/src/test/java/ru/nsu/alowator/CalculatorTest.java
@@ -1,32 +1,91 @@
 package ru.nsu.alowator;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import java.util.Scanner;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
-    Calculator calculator;
-
-    @BeforeEach
-    void calculator() {
-        calculator = new Calculator();
-    }
-
     @Test
     void calculate_value() {
-        assertEquals(10.5, calculator.calculate("10.5"));
-    }
-
-    @Test
-    void calculate_negativeValue() {
-        assertEquals(-10.0, calculator.calculate("-10"));
+        assertEquals(-10.5, Calculator.calculate("-10.5"));
     }
 
     @Test
     void calculate_add() {
-        assertEquals(8, calculator.calculate("+ 5 3"));
+        assertEquals(0, Calculator.calculate("+ -100 + 40 60"));
+    }
+
+    @Test
+    void calculate_sub() {
+        assertEquals(972, Calculator.calculate("- - - - 1000 7 7 7 7"));
+    }
+
+    @Test
+    void calculate_mul() {
+        assertEquals(-50, Calculator.calculate("* -1 * 5 10"));
+    }
+
+    @Test
+    void calculate_div() {
+        assertEquals(50, Calculator.calculate("/ 100 / 10 5"));
+    }
+
+    @Test
+    void calculate_divByZero() {
+        assertEquals(Double.POSITIVE_INFINITY, Calculator.calculate("/ 100 0"));
+    }
+
+    @Test
+    void calculate_log() {
+        assertEquals(0.4234226524603038, Calculator.calculate("log log log 100"));
+    }
+
+    @Test
+    void calculate_logNegative() {
+        assertEquals(Double.NaN, Calculator.calculate("log log log -123"));
+    }
+
+    @Test
+    void calculate_pow() {
+        assertEquals(1024.0, Calculator.calculate("pow 2 10"));
+    }
+
+    @Test
+    void calculate_sqrt() {
+        assertEquals(228, Calculator.calculate("sqrt 51984"));
+    }
+
+    @Test
+    void calculate_sqrtMul() {
+        assertEquals(228, Calculator.calculate("sqrt * 228 228"));
+    }
+
+    @Test
+    void calculate_sin() {
+        assertEquals(0.8414709848078965, Calculator.calculate("sin 1"));
+    }
+
+    @Test
+    void calculate_cos() {
+        assertEquals(-0.4161468365471424, Calculator.calculate("cos 2"));
+    }
+
+    @Test
+    void calculate_expression() {
+        assertEquals(
+                259.7466245647059,
+                Calculator.calculate("* log 10 + sqrt 100 + * 26 4 - / 8 + sin 0 cos 1 pow 2 4")
+        );
+    }
+
+    @Test
+    void calculate_expressionScanner() {
+        Scanner scanner = new Scanner("* log 10 + sqrt 100 + * 26 4 - / 8 + sin 0 cos 1 pow 2 4");
+        assertEquals(
+                259.7466245647059,
+                Calculator.calculate(scanner)
+        );
     }
 
 }
