858acc210c59065b244640eb8d2174c558dbab01
Mark Bukhner <markbuxner@mail.ru>
2021-11-29 20:50:02 +0700

    Complete Task_1_4_2

--- Task_1_4_2/src/main/java/ru/nsu/alowator/notebook/Note.java
+++ Task_1_4_2/src/main/java/ru/nsu/alowator/notebook/Note.java
@@ -1,6 +1,11 @@
 package ru.nsu.alowator.notebook;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import com.google.gson.Gson;
 
 public class Note {
 
@@ -26,8 +31,26 @@ public class Note {
         return datetime;
     }
 
+    public boolean containsAnyOf(List<String> keywords) {
+        keywords = keywords.stream().map(String::toLowerCase).toList();
+        String lowerName = name.toLowerCase();
+        String lowerText = text.toLowerCase();
+
+        for (String keyword : keywords) {
+            if (lowerText.contains(keyword) || lowerName.contains(keyword)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static DatetimeNoteComparator datetimeComparator() {
         return new DatetimeNoteComparator();
     };
 
+    @Override
+    public String toString() {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy kk:mm");
+        return name + " (" + formatter.format(datetime) + "):\n" + text;
+    }
 }
--- Task_1_4_2/src/main/java/ru/nsu/alowator/notebook/Notebook.java
+++ Task_1_4_2/src/main/java/ru/nsu/alowator/notebook/Notebook.java
@@ -1,17 +1,31 @@
 package ru.nsu.alowator.notebook;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import ru.nsu.alowator.timer.Timer;
 
+import java.lang.reflect.Type;
 import java.util.*;
 
 public class Notebook {
 
-    private Timer timer;
-    private List<Note> notes;
+    private final Timer timer;
+    private final List<Note> notes;
 
     public Notebook(Timer timer) {
         this.timer = timer;
         this.notes = new ArrayList<>();
+    }
+
+    public Notebook(Timer timer, String jsonNotebookDump) {
+        this.timer = timer;
+        Type listOfNotes = new TypeToken<ArrayList<Note>>(){}.getType();
+        List<Note> loadedNotes = new Gson().fromJson(jsonNotebookDump, listOfNotes);
+        if (loadedNotes == null) {
+            loadedNotes = new ArrayList<>();
+        }
+        this.notes = loadedNotes;
     }
 
     public void add(String name, String text) {
@@ -27,11 +41,17 @@ public class Notebook {
         return notes.stream().sorted(Note.datetimeComparator()).toList();
     }
 
-    public List<Note> getNotesSortedByDateBetween(Date start, Date end) {
+    public List<Note> getNotesSortedByDateBetweenContainsAnyOf(Date start, Date end, List<String> keywords) {
         return notes.stream()
                 .filter(note -> note.getDatetime().after(start) && note.getDatetime().before(end))
+                .filter(note -> note.containsAnyOf(keywords))
                 .sorted(Note.datetimeComparator())
                 .toList();
+    }
+
+    public String toJson() {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(this.notes);
     }
 
 }
