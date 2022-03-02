7415fc09f1b1ff19bdf7c4ed7961e89ba83743d8
Mark Bukhner <markbuxner@mail.ru>
2022-03-03 04:28:59 +0700

    Final varsion (believe)

--- Task_2_1_1/src/main/java/ru/nsu/alowator/Calculus.java
+++ Task_2_1_1/src/main/java/ru/nsu/alowator/Calculus.java
@@ -2,9 +2,14 @@ package ru.nsu.alowator;
 
 public class Calculus {
 
-    public static boolean isPrime(Integer x) {
-        for (int i = 2; i * i <= x; i++) {
-            if (x % i == 0)
+    public static boolean isPrime(int x) {
+        if (x <= 0)
+            throw new IllegalArgumentException("x must be natural");
+        else if (x == 1)
+            return false;
+
+        for (long i = 2; i * i <= (long) x; i++) {
+            if ((long) x % i == 0)
                 return false;
         }
         return true;
