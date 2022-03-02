e0470099e3c8081d104223c510109101889feef4
Mark Bukhner <markbuxner@mail.ru>
2022-03-03 04:51:38 +0700

    Add javadoc

--- Task_2_1_1/src/main/java/ru/nsu/alowator/impl/NotPrimeFinder.java
+++ Task_2_1_1/src/main/java/ru/nsu/alowator/impl/NotPrimeFinder.java
@@ -8,10 +8,20 @@ public class NotPrimeFinder implements Callable<Boolean> {
 
     protected final Integer[] array;
 
+    /**
+     * Base singlethread implementation,
+     * other implementations must be inherited from this.
+     *
+     * @param array of integers, that will be scanned for
+     * the presence of non-prime numbers by calling {@code call()}
+     */
     public NotPrimeFinder(Integer[] array) {
         this.array = array;
     }
 
+    /**
+     * @return {@code true} if contains non-prime numbers
+     */
     @Override
     public Boolean call() {
         for (Integer x : array) {
